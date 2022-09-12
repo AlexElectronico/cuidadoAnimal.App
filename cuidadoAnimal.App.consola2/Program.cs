27b0b89ee34237b439ae23f4bb8ee283685dbeef
Alex <alexfabiancade@hotmail.com>
2022-09-12 15:36:35 -0500

    ver2.1

---
+++ cuidadoAnimal.App.consola2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using cuidadoAnimal.App.dominio;
+using cuidadoAnimal.App.persistencia;
+
+namespace cuidadoAnimal.App.consola2
+{
+   class Program
+
+   {
+    private static IrepositorioPropietario _repoPropietario=new RepositorioPropietario(new persistencia.AppContext());
+    static void Main(string[] args)
+    {
+     Console.WriteLine("HOLA MUNDO");
+     AddPropietario();
+    }
+    
+    private static void AddPropietario()
+    {
+      var propietario=new Propietario()
+      {
+        // IdPersona=11,
+         Nombres="David",
+         Apellidos="ospina",
+         Telefono="400404",
+         NombreHacienda=3333,
+         Email="ospi@ww",
+         Direccion="av 456"
+      };  
+      _repoPropietario.AddPropietario(propietario);
+
+    }
+
+
+   } 
+}
--- cuidadoAnimal.App.presentacion/Pages/Clientes/Lista.cshtml.cs
+++ cuidadoAnimal.App.presentacion/Pages/Clientes/Lista.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using cuidadoAnimal.App.dominio;
+using cuidadoAnimal.App.persistencia;
+using Microsoft.AspNetCore.Authorization;
+//librerias opcionales
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace cuidadoAnimal.App.presentacion.Pages
 {
     public class ListaModel : PageModel
     {
-        public void OnGet()
+
+        private readonly IrepositorioPropietario repositorioPropietario;
+        public IEnumerable<Propietario> Propietario{set;get;} 
+
+        public ListaModel()
         {
+         this.repositorioPropietario= new RepositorioPropietario(new cuidadoAnimal.App.persistencia.AppContext());  
+        }
+
+        public void OnGet(string filtroBusqueda)
+        {
+            Propietario=repositorioPropietario.GetAllPropietarios();
+           
+           
+         
         }
     }
 }
